019d22ec916a42ed40f81ca10e6b3b81689997ed
Dan1l0s <danilos01pr@gmail.com>
2023-02-24 21:00:31 +0300

    Implemented bot status and 'help' command

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ FFMPEG_OPTIONS = {
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
 vcs = {}
-bot = commands.Bot(command_prefix="?", intents=disnake.Intents.all())
-
-
-@bot.remove_command('help')
-@bot.command()
-async def help(ctx):
-    ctx.send("Тебе уже ничего не поможет")
+bot = commands.Bot(command_prefix="?", intents=disnake.Intents.all(), activity=disnake.Game(name="/help"))
 
 
 @bot.event
@@ -25,14 +19,19 @@ async def on_ready():
     print("Bot is turned on")
 
 
-@bot.slash_command(description="Test slash-command")
-async def test(interaction: disnake.AppCmdInter, num: int):
-    await interaction.send(f"test {num}")
+@bot.remove_command('help')
+@bot.command()
+async def help(ctx):
+    await ctx.send("Тебе уже ничего не поможет")
+
+
+# @bot.slash_command(description="Test slash-command")
+# async def test(interaction: disnake.AppCmdInter, num: int):
+#     await interaction.send(f"test {num}")
 
 
 @bot.slash_command(description="Plays a song from youtube (paste URL or type a query)")
 async def play(ctx: disnake.AppCmdInter, url: str):
-    print(url)
     if ctx.author.voice:
         try:
             vc = await ctx.author.voice.channel.connect()
@@ -84,15 +83,19 @@ async def stop(ctx: disnake.AppCmdInter):
         await ctx.send("I am not playing anything!")
 
 
-@bot.command()
-async def clear(ctx, amount: int):
-    await ctx.channel.purge(limit=amount+1)
-    await ctx.send(f"Deleted {amount} messages")
+@bot.slash_command(description="Reviews list of commands")
+async def help(ctx: disnake.AppCmdInter):
+    await ctx.send(embed=disnake.Embed(description="Type /play to order a song (use URL from YT or just type the song's name)\nType /stop to stop playback\nType /pause to pause playback\nType /resume to resume playback"))
+
+
+# @bot.command()
+# async def clear(ctx, amount: int):
+#     await ctx.channel.purge(limit=amount+1)
+#     await ctx.send(f"Deleted {amount} messages")
 
 
 @bot.command()
 async def play(ctx, url: str):
-    print(url)
     if ctx.message.author.voice:
         try:
             vc = await ctx.author.voice.channel.connect()
@@ -121,7 +124,8 @@ async def pause(ctx):
     try:
         vcs[ctx.guild.id].pause()
         await ctx.send("Player paused!")
-    except Exception as err:
+    except Exception as error:
+        print(error)
         await ctx.send("I am not playing anything!")
 
 
@@ -130,7 +134,8 @@ async def resume(ctx):
     try:
         vcs[ctx.guild.id].resume()
         await ctx.send("Player resumed!")
-    except Exception as err:
+    except Exception as error:
+        print(error)
         await ctx.send("There's nothing to play!")
 
 
@@ -140,7 +145,8 @@ async def stop(ctx):
         vcs[ctx.guild.id].stop()
         await vcs[ctx.guild.id].disconnect()
         await ctx.send("DJ decided to stop!")
-    except Exception as err:
+    except Exception as error:
+        print(error)
         await ctx.send("I am not playing anything!")
 
 bot.run(config.token)
