d3bae17fd2d1433c0b4ccc3185615914630b4da8
GilmartinR <gilmartin.sasha@gmail.com>
2023-03-24 14:19:17 +0300

    Minor changes to time format

--- logger.py
+++ logger.py
@@ -12,7 +12,7 @@ script_dir = os.path.dirname(__file__)
 rel_path = "logs/" + file_name
 abs_path = os.path.join(script_dir, rel_path)
 
-# Logger
+#Logger
 
 class logger:
     def __init__(self, vcs, songs_queue):
@@ -22,41 +22,41 @@ class logger:
     def error(self, err):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : ERROR :" + err)
+            datetime.datetime.now().strftime("%H:%M:%S") + " : ERROR :" + err)
         f.close()
 
     def skip(self, ctx):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : SKIP : Skipped track at {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
+            datetime.datetime.now().strftime("%H:%M:%S") + " : SKIP : Skipped track at {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
         f.close()
 
     def enabled(self):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : Bot is On\n")
+            datetime.datetime.now().strftime("%H:%M:%S") + " : Bot is On\n")
         f.close()
 
     def logged(self, entry):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : AUDIT_LOG : {entry.user} did {entry.action} to {entry.target}\n")
+            datetime.datetime.now().strftime("%H:%M:%S") + f" : AUDIT_LOG : {entry.user} did {entry.action} to {entry.target}\n")
         f.close()
 
     def added(self, info, ctx):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : PLAY : Added {info['title']} to queue with duration of {helpers.get_duration(self.songs_queue[ctx.guild.id][0]['duration'])}\n")
+            datetime.datetime.now().strftime("%H:%M:%S") + f" : PLAY : Added {info['title']} to queue with duration of {helpers.get_duration(self.songs_queue[ctx.guild.id][0]['duration'])}\n")
         f.close()
 
     def playing(self, ctx):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : PLAY : Playing {self.songs_queue[ctx.guild.id][0]['title']} in vc: {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
+           datetime.datetime.now().strftime("%H:%M:%S") + f" : PLAY : Playing {self.songs_queue[ctx.guild.id][0]['title']} in vc: {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
         f.close()
 
     def finished(self, ctx):
         f = open(f'{abs_path}.txt', "a")
         f.write(
-            f"{datetime.datetime.now()} : STOP : Finished playing in vc: {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
+            datetime.datetime.now().strftime("%H:%M:%S") + f" : STOP : Finished playing in vc: {ctx.guild.name} / {self.vcs[ctx.guild.id].channel}\n")
         f.close()
