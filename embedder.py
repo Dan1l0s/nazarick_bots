d5e8fc9f6dc650d0dadb1acecb37ec3908994157
GilmartinR <gilmartin.sasha@gmail.com>
2023-03-28 17:40:58 +0300

    'voice_update', guild/member action separation

--- embedder.py
+++ embedder.py
@@ -20,32 +20,43 @@ class embed:
                         value=helpers.get_nickname(ctx.author), inline=True)
         return embed
     
-    def action(self, entry): #WHITE
-        embed = disnake.Embed(
-            title=f'`{entry.user}` did `{entry.action}` to `{entry.target}`',
-            color=disnake.Colour.from_rgb(255, 255, 255),
-            timestamp=datetime.datetime.now())
+    def action(self, entry): #White or SkyBlue
+        if "member" in f'{entry.action}':
+            embed = disnake.Embed(
+                title=f'`{entry.user}` did `{entry.action}` to `{entry.target}`'.replace('AuditLogAction.', ''),
+                color=disnake.Colour.from_rgb(255, 255, 255),
+                timestamp=datetime.datetime.now())
+        else:
+            embed = disnake.Embed(
+                title=f'`{entry.user}` did `{entry.action}` to `{entry.target}`'.replace('AuditLogAction.', ''),
+                color=disnake.Colour.from_rgb(160, 220, 255),
+                timestamp=datetime.datetime.now())
         return embed
     
-    def switched(self, member, before, after): #PINK
+    def switched(self, member, before, after): #Pink
         embed = disnake.Embed(
             title=f'`{helpers.get_nickname(member)}` switched from `{before.channel.name}` to `{after.channel.name}`',
             color=disnake.Colour.from_rgb(255, 180, 255),
             timestamp=datetime.datetime.now())
         return embed
     
-    def connected(self, member, after): #GREEN:
+    def connected(self, member, after): #Green
         embed = disnake.Embed(
             title=f"`{helpers.get_nickname(member)}` connected to `{after.channel.name}`",
             color=disnake.Colour.from_rgb(0, 255, 0),
             timestamp=datetime.datetime.now())
         return embed
     
-    def disconnected(self, member, before): #RED
+    def disconnected(self, member, before): #Red
         embed = disnake.Embed(
             title=f'`{helpers.get_nickname(member)}` disconnected from `{before.channel.name}`',
             color=disnake.Colour.from_rgb(255, 0, 0),
             timestamp=datetime.datetime.now())
         return embed
     
-    
+    def voice_update(self, member): #Yellow
+        embed = disnake.Embed(
+            title=f'`{helpers.get_nickname(member)}` updated voice state',
+            color=disnake.Colour.from_rgb(255, 255, 0),
+            timestamp=datetime.datetime.now())
+        return embed
