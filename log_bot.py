38e2e34f8a5e4d72c1675a9f77c427ee749d4b02
GilmartinR <gilmartin.sasha@gmail.com>
2023-06-20 18:54:31 +0300

    Extended logger bot functionality

--- log_bot.py
+++ log_bot.py
@@ -8,6 +8,29 @@ import dicts
 import helpers
 import config
 
+gl_flag = True
+
+class Activity():
+    acttype = None
+    actname = None
+    def __init__(self, acttype = None, actname = None):
+        self.acttype = acttype
+        self.actname = actname
+    def __eq__(self, other):
+        return self.acttype == other.acttype and self.actname == other.actname
+
+class UserStatus():
+
+    status = None
+    activities = []
+    def __init__(self, status):
+        self.status = status
+        self.activities = []    
+    def __eq__(self, other):
+        a = set((x.acttype, x.actname) for x in self.activities)
+        b = set((x.acttype, x.actname) for x in other.activities)
+        return self.status == other.status and a == b
+    
 class AutoLog():
     
     name = None
@@ -122,13 +145,85 @@ class AutoLog():
         async def on_ready():
             self.logger.enabled(self.bot)
             print(f"{self.name} is logged as {self.bot.user}")
+            await self.status_check()
 
         @self.bot.event
         async def on_disconnect():
             self.logger.lost_connection(self.bot)
             print(f"{self.name} has disconnected from Discord")
+            global gl_flag
+            gl_flag = False
+
+    # --------------------- SLASH COMMANDS --------------------------------
+
+        @self.bot.slash_command(description="Chech current status of user")
+        async def status(inter: disnake.AppCmdInter, member: disnake.Member):
+            if await self.check_dm(inter):
+                return
+            await inter.send(embed = self.embeds.get_status(member))         
+
+        @self.bot.slash_command(description="Stop checking current status of user")
+        async def stopstatus(inter: disnake.AppCmdInter):
+            if await self.check_dm(inter):
+                return
+            await inter.send("Stopping status_check running")
+            global gl_flag
+            gl_flag = False     
+
+        @self.bot.slash_command(description="Check current status of users")
+        async def startstatus(inter: disnake.AppCmdInter):
+            if await self.check_dm(inter):
+                return
+            await inter.send("Starting status_check running")
+            await self.status_check()         
+
+    # --------------------- METHODS --------------------------------
 
     async def run(self):
         await self.bot.start(config.tokens[self.name])
-        
+
+    async def check_dm(self, inter):
+        if not inter.guild:
+            if inter.author.id in config.admin_ids[569924343010689025]:
+                await inter.send(f"{dicts.dm_error_admin}")
+            else:
+                await inter.send(f"{dicts.dm_error}")
+            return True
+        return False   
     
+    async def status_check(self):
+        guild_list = self.bot.guilds
+        global gl_flag
+        gl_flag = True
+        print("STARTED STATUS TRACKING")
+        while True:
+            for x in range(len(guild_list)):
+                if guild_list[x].id in config.log_ids:
+                    old_list = self.gen_status_and_activity_list(guild_list[x].members)
+                    await asyncio.sleep(0.25)
+                    new_list = self.gen_status_and_activity_list(guild_list[x].members)
+                    #print(id(new_list))
+                    if len(new_list) == len(old_list):
+                        for y in range(len(new_list)):
+                            if old_list[y] != new_list[y] and not (guild_list[x].members[y].bot and guild_list[x].members[y].id not in config.bot_ids.values()):
+                                if old_list[y].status != new_list[y].status:
+                                    self.logger.status_upd(guild_list[x].members[y])
+                                if old_list[y].activities != new_list[y].activities:
+                                    self.logger.activity_upd(guild_list[x].members[y], old_list[y], new_list[y])    
+                                await guild_list[x].members[y].guild.get_channel(config.log_ids[guild_list[x].id]).send(embed=self.embeds.activity_update(guild_list[x].members[y], old_list[y], new_list[y]))
+            if not gl_flag:
+                print("STOPPED STATUS TRACKING")
+                break
+
+    def gen_status_and_activity_list(self, list):
+        newlist = []
+        for elem in list:
+            new_user = UserStatus(str(elem.status))
+            for acts in elem.activities:
+                if f"{type(acts)}" == "<class 'disnake.activity.Spotify'>":
+                    new_act = Activity(type(acts), f'{acts.artists[0]} - "{acts.title}"')
+                elif f"{type(acts)}" != "<class 'NoneType'>":
+                    new_act = Activity(type(acts), f'{acts.name}')
+                new_user.activities.append(new_act)
+            newlist.append(new_user)
+        return newlist
