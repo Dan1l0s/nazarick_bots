d3afacd06889b3063b543d4a24c28a7992a7a738
Mag1str0 <rysin_d@mail.ru>
2023-07-03 14:15:16 +0300

    Added install script

--- main.py
+++ main.py
@@ -8,27 +8,18 @@ from log_bot import AutoLog
 
 
 async def main():
+    
     process_pool = pool.ProcessPoolExecutor()
     file_logger = FileLogger(True)
 
-    music_leader = MusicBotLeader("music_main", file_logger, process_pool)
-    music_instance1 = MusicBotInstance("music_assistant1", file_logger, process_pool)
-    music_instance2 = MusicBotInstance("music_assistant2", file_logger, process_pool)
-    music_instance3 = MusicBotInstance("music_assistant3", file_logger, process_pool)
-
+    music_leader = MusicBotLeader("eclair1", file_logger, process_pool)
+    music_instance1 = MusicBotInstance("eclair2", file_logger, process_pool)
     music_leader.add_instance(music_instance1)
-    music_leader.add_instance(music_instance2)
-    music_leader.add_instance(music_instance3)
 
-    log_bot = AutoLog("logs", file_logger)
 
     tasks = []
     tasks.append(music_leader.run())
-
     tasks.append(music_instance1.run())
-    tasks.append(music_instance2.run())
-    tasks.append(music_instance3.run())
-    tasks.append(log_bot.run())
 
     await asyncio.gather(*tasks)
 
